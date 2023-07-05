2a098adfde32a8080e642970c754425298145960
Leandro <atiladevelop@gmail.com>
2023-07-04 21:43:51 -0300

    Pie chart added

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,19 +1,61 @@
+import 'package:dashboard/components/chart.dart';
 import 'package:dashboard/components/header.dart';
 import 'package:dashboard/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: SingleChildScrollView(
-        padding: EdgeInsets.all(defaultPadding),
+        padding: const EdgeInsets.all(defaultPadding),
         child: Column(
           children: [
-            Header(),
+            const Header(),
+            const SizedBox(
+              height: defaultPadding,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    height: 500,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  width: defaultPadding,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    padding: const EdgeInsets.all(defaultPadding),
+                    height: 500,
+                    decoration: const BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: const Column(
+                      children: [
+                        Text(
+                          'Storage Details',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Chart()
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
